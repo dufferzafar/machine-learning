f54534de541381eb385a3189dca9525549e3ff7a
Shadab Zafar <dufferzafar0@gmail.com>
2018-02-12 23:59:52 +0530

    Q1: Plots! part c,d,e

--- Assignment 1/q1_linear.py
+++ Assignment 1/q1_linear.py
@@ -31,19 +31,20 @@ def part_a(eta=0.001):
     converged = False
 
     # Error values that the algorithm goes through (used for plotting)
-    J_values = []
+    J_trace = np.array([])
 
     print("Learning Rate: ", eta)
     print("Initial Error: ", Jn)
 
     while (not converged):
+        J_trace = np.append(J_trace, [theta[0], theta[1], Jn])
+
         # The gradient descent update rule
         theta -= eta * (X @ theta - y) @ X
 
         # Calculate new error and check convergence
         Jp = Jn
         Jn = J(theta)
-        J_values.append((theta, Jn))
 
         if abs(Jp - Jn) < 10 ** -15:
             converged = True
@@ -60,12 +61,14 @@ def part_a(eta=0.001):
     print("Number of iterations: ", iters)
     print("Stopping Criteria: (J_new - J_old) < 10 ** -15")
 
-    return theta, J_values
+    return theta, J_trace.reshape(iters, 3)
 
 
 def part_b(theta):
-    data, = plt.plot(X[:, 1], y, 'rx', label="Data")
+
     yy = list(map(lambda x: theta @ x, X))
+
+    data, = plt.plot(X[:, 1], y, 'rx', label="Data")
     line, = plt.plot(X[:, 1], yy, 'b', label="Hypothesis")
 
     plt.xlabel("Acidity of wine (normalised)")
@@ -77,9 +80,9 @@ def part_b(theta):
     plt.show()
 
 
-def part_c():
+def part_c(Jt=None):
     # Create a mesh using numpy awesome-sauce!
-    T0, T1 = np.mgrid[-1:3:50j, -2:2.5:50j]
+    T0, T1 = np.mgrid[0:2:50j, -1:1:50j]
     mesh = np.c_[T0.flatten(), T1.flatten()]
 
     # Compute J_values for the grid
@@ -96,14 +99,18 @@ def part_c():
     ax.set_ylabel(r'$\theta_1$', labelpad=10)
     ax.set_zlabel(r'$J(\theta)$', labelpad=10)
 
+    if Jt is not None:
+        ax.plot(Jt[:, 0], Jt[:, 1], Jt[:, 2], linestyle='-',
+                color='r', marker='o', markersize=2.5)
+
     save(plt, "q1_a.png")
 
     # TODO: 0.2 second time gap?
     plt.show()
 
 
-def part_d():
-    T0, T1 = np.mgrid[-1:11:0.1, -1:11:0.1]
+def part_d(Jt=None):
+    T0, T1 = np.mgrid[0:2:50j, -1:1:50j]
     mesh = np.c_[T0.flatten(), T1.flatten()]
 
     J_values = (
@@ -112,33 +119,43 @@ def part_d():
     )
 
     # TODO: 0.2 second time gap?
-    plt.contour(T0, T1, J_values, np.arange(0, 50, 3), colors="k")
-    plt.contour(T0, T1, J_values, colors="k")
+    # plt.contour(T0, T1, J_values, np.arange(0, 50, 3), colors="k")
+    plt.contour(T0, T1, J_values, 25, colors="k")
+    plt.xlabel(r'$\theta_0$', labelpad=10)
+    plt.ylabel(r'$\theta_1$', labelpad=10)
+
+    if Jt is not None:
+        plt.plot(Jt[:, 0], Jt[:, 1], linestyle='-',
+                 color='r', marker='o', markersize=2.5)
 
     return plt
 
 
 def part_e():
+    # for eta in [0.021, 0.025]:
     for eta in [0.001, 0.005, 0.009, 0.013, 0.017, 0.021, 0.025]:
         print("\n --- \n Eta: %.2f \n --- \n" % eta)
 
-        theta, J_values = part_a(eta)
+        theta, J_trace = part_a(eta)
 
-        plt = part_d()
+        plt = part_d(J_trace)
+        plt.title(r"$Contours (\eta=$" + str(eta) + ")")
 
-        for t, j in J_values:
-            plt.plot([t[0]], [t[1]], markerfacecolor='r',
-                     marker='o', markersize=3)
-
-        plt.show()
+        save(plt, "q1_e_%.3f.png" % eta)
+        plt.close()
+        # plt.show()
 
 
 if __name__ == '__main__':
-    theta, _ = part_a()
+    theta, J_trace = part_a(eta=0.001)
     part_b(theta)
-    # part_c()
 
-    # plt = part_d()
-    # plt.show()
+    part_c()
+    part_c(J_trace)
 
-    # part_e()
+    plt = part_d(J_trace)
+    save(plt, "q1_d_0.001.png")
+    plt.close()
+    plt.show()
+
+    part_e()
