d1ef192e28abc0a8ad917506aab2854e8caab8b0
Shadab Zafar <dufferzafar0@gmail.com>
2018-03-10 22:46:50 +0530

    libsvm.sh: CV & Test accuracies for all C

--- 2/libsvm.sh
+++ 2/libsvm.sh
@@ -31,8 +31,15 @@ libsvm/svm-predict data/mnist/test-svm-scaled models/gaussian-model-scaled gauss
 
 # Part D
 
-libsvm/svm-train -q -s 0 -c 0.00001 -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
-libsvm/svm-train -q -s 0 -c 0.001   -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
-libsvm/svm-train -q -s 0 -c 1       -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
-libsvm/svm-train -q -s 0 -c 5       -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
-libsvm/svm-train -q -s 0 -c 10      -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
+# Cross Validation Accuracies
+for cval in 0.00001 0.001 1 5 10; do
+    echo $cval
+    libsvm/svm-train -q -s 0 -c $cval -t 2 -g 0.05 -v 10 data/mnist/train-svm-scaled
+done
+
+# Test Data Accuracies
+for cval in 0.00001 0.001 1 5 10; do
+    echo $cval
+    libsvm/svm-train -q -s 0 -c $cval -t 2 -g 0.05 data/mnist/train-svm-scaled models/gaussian-model-scaled-$cval
+    libsvm/svm-predict data/mnist/test-svm-scaled models/gaussian-model-scaled-$cval gaussian-test-labels
+done
