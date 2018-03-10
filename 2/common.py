b222972d9999ab9b2746b684107e1d6006693bc3
Shadab Zafar <dufferzafar0@gmail.com>
2018-03-10 22:45:55 +0530

    Add method to create confusion matrix

--- 2/common.py
+++ 2/common.py
@@ -11,3 +11,29 @@ def accuracy(actual, predicted):
 
     correct = sum([a == p for (a, p) in zip(actual, predicted)])
     return correct / len(actual)
+
+
+def make_confusion(actual, predicted):
+    """Create a confusion matrix from two list of labels."""
+
+    assert len(actual) == len(predicted)
+
+    # TODO: Not proud of this code - Make Pythonic!
+
+    # Using a dict ensures that classes can be anything
+    cm = {
+        c: {cc: 0 for cc in set(actual)}
+        for c in set(actual)
+    }
+
+    # Update entries with counts
+    for a, p in zip(actual, predicted):
+        cm[a][p] += 1
+
+    # Now convert into a matrix form
+    # TODO: See if seaborn can take dictionaries as input
+    cmm = []
+    for a in sorted(cm):
+        cmm.append([cm[a][p] for p in sorted(cm[a])])
+
+    return cmm
