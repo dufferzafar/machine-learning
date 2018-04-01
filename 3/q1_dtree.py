b77f7cdd728df26e2ef839c518961c87073b32e7
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-01 20:25:19 +0530

    3: dtree cleanup

--- 3/q1_dtree.py
+++ 3/q1_dtree.py
@@ -1,22 +1,34 @@
+"""
+Assignment 3 Question 1: Decision Trees.
 
-import graphviz
+Part A: Decision Tree ID3 / Entropy
+Part B: Post Pruning the Tree
+Part C: Dynamic Median Calculations
+Part D: sklearn's Decision Tree
+Part E: sklearn's Random Forest
+"""
 
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import ParameterGrid
+from collections import namedtuple
 
+import numpy as np
 from matplotlib import pyplot as plt
 
-from read_data import attributes, preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import ParameterGrid
+from sklearn.ensemble import RandomForestClassifier
+
+from read_data import preprocess
+
+
+# TODO: Move this data reading into a function
+train_data = preprocess("data/train.csv")
+test_data = preprocess("data/test.csv")
+valid_data = preprocess("data/valid.csv")
 
 
 def part_d():
 
-    # Read the data
-    train_data = preprocess("data/train.csv")
-    # test_data = preprocess("data/test.csv")
-    valid_data = preprocess("data/valid.csv")
-
+    # Run with default parameters
     clf = DecisionTreeClassifier(criterion="entropy", random_state=0)
     clf.fit(train_data[:, 1:], train_data[:, 0])
 
@@ -29,7 +41,8 @@ def part_d():
     accuracy = []
     depths = range(3, 30)
     for d in depths:
-        clf = DecisionTreeClassifier(criterion="entropy", max_depth=d, random_state=0)
+        clf = DecisionTreeClassifier(criterion="entropy", random_state=0,
+                                     max_depth=d)
         clf.fit(train_data[:, 1:], train_data[:, 0])
         accuracy.append(100 * clf.score(valid_data[:, 1:], valid_data[:, 0]))
 
@@ -39,19 +52,6 @@ def part_d():
     plt.ylabel("Validation accuracy")
     plt.show()
 
-    # Sort features by their relative importance
-    # list(sorted(zip(attributes, clf.feature_importances_), key=lambda x: x[1]))
-
-    # Export the tree in graphviz format
-    # dot_data = tree.export_graphviz(
-    # clf, out_file="default_tree.dot",
-    # feature_names=attributes[1:],
-    # class_names=["poor", "rich"],
-    # filled=True, rounded=True,
-    # special_characters=True
-    # )
-    # graphviz.Source(dot_data)
-
     # Range of parameters to find best accuracy over
     parameters = {
         'max_depth': range(8, 13),
@@ -59,21 +59,18 @@ def part_d():
         'min_samples_leaf': range(5, 75, 5),
     }
 
-    # Use cross-validation on training set to find best parameters
-    # clf = GridSearchCV(DecisionTreeClassifier(criterion="entropy"), parameters)
-    # clf.fit(train_data[:, 1:], train_data[:, 0])
-    # clf.best_params_
-
     # Run a custom search that scores on the validation set
     results = {}
     for param in ParameterGrid(parameters):
-        clf = DecisionTreeClassifier(criterion="entropy", random_state=0, **param)
+        clf = DecisionTreeClassifier(criterion="entropy", random_state=0,
+                                     **param)
         clf.fit(train_data[:, 1:], train_data[:, 0])
 
         validation_score = clf.score(valid_data[:, 1:], valid_data[:, 0])
         results[validation_score] = param
 
-    print("Parameters resultin in accuracy %r" % max(results), results[max(results)])
+    print("Parameters resulting in accuracy %r" %
+          max(results), results[max(results)])
 
 
 def part_e():
@@ -87,13 +84,15 @@ def part_e():
 
     results = {}
     for param in ParameterGrid(parameters):
-        clf = RandomForestClassifier(criterion='entropy', random_state=0, **param)
+        clf = RandomForestClassifier(criterion='entropy', random_state=0,
+                                     **param)
         clf.fit(train_data[:, 1:], train_data[:, 0])
 
         validation_score = clf.score(valid_data[:, 1:], valid_data[:, 0])
         results[validation_score] = param
 
-    print("Parameters resultin in accuracy %r" % max(results), results[max(results)])
+    print("Parameters resulting in accuracy %r" %
+          max(results), results[max(results)])
 
 
 if __name__ == '__main__':
