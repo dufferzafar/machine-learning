0d853ee251c7808ebfdd9a71112d3d861f978b85
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-03 13:14:42 +0530

    Separate module for decision tree

---
+++ 3/decision_tree.py
@@ -0,0 +1,148 @@
+import numpy as np
+
+from collections import namedtuple
+from common import accuracy
+
+
+# Internal (decision) node of the tree
+Node = namedtuple("Node", [
+    "attr_idx",  # Index of the attribute to make decision on
+    "children",  # A dictionary of attribute values and child nodes
+    "cls",       # Majority class at this node
+])
+
+# Leaf nodes
+Leaf = namedtuple("Leaf", [
+    "cls",       # Majority class at this leaf
+])
+
+
+def entropy(Y):
+    """
+    Calculate entropy from class labels.
+    """
+
+    _, counts = np.unique(Y, return_counts=True)
+    probabilities = counts.astype('float') / len(Y)
+
+    return -1 * sum(p * np.log2(p) for p in probabilities if p)
+
+
+def partition(Xa):
+    """
+    Partition a column based on the unique values it takes.
+
+    { value: [indices where that value occurs in the column] }
+    """
+    return {v: np.where(Xa == v)[0] for v in np.unique(Xa)}
+
+
+class DecisionTree():
+
+    def __init__(self, data):
+        self.root = self._build_tree(data)
+
+    @staticmethod
+    def _build_tree(data):
+        """
+        Build a decision tree using ID3 / information gain.
+
+        First column (data[:, 0]) is the output class.
+        """
+
+        Y = data[:, 0]
+        majority_class = np.bincount(Y).argmax()
+
+        # if data is "pure" i.e has examples of a single class
+        # then return a leaf node predicting that class
+        if len(set(Y)) <= 1:
+            return Leaf(cls=majority_class)
+
+        # if all features finished?
+        # TODO: Will info gain handle attribute repetitions?
+
+        # Find the attribute that maximizes the gain
+        gain, attr_idx = DecisionTree._best_attribute(data)
+
+        if gain > 0:
+            # Split if gain is positive
+            children = {v: DecisionTree._build_tree(data[p])
+                        for v, p in partition(data[:, attr_idx]).items()}
+
+            return Node(attr_idx, children, majority_class)
+        else:
+            # Otherwise create a leaf node that predicts the majority class
+            return Leaf(cls=majority_class)
+
+    @staticmethod
+    def _best_attribute(data):
+        """
+        Use information gain to decide which attribute to split on.
+        """
+
+        # Need to find these parameters
+        best_gain = -1
+        best_attr = -1
+
+        Y = data[:, 0]
+        X = data[:, 1:]
+
+        # Iterate over each attribute
+        for i, Xa in enumerate(X.T):
+
+            # Create partitions over this attribute
+            entropy_Y_Xa = sum((len(p) / len(Xa)) * entropy(Y[p])
+                               for p in partition(Xa).values())
+
+            gain = entropy(Y) - entropy_Y_Xa
+
+            # TODO: In case of a tie, choose the attribute which appears first in the
+            # ordering as given in the training data.
+            if gain > best_gain:
+                best_gain = gain
+
+                # NOTE: +1 because the data contains output variables at 1st column
+                # so attributes/features start from 2nd column
+                best_attr = i + 1
+
+        return best_gain, best_attr
+
+    @staticmethod
+    def _predict(dtree, x):
+        """Predict a single example using dtree."""
+        if isinstance(dtree, Leaf):
+            return dtree.cls
+        else:
+            child = dtree.children.get(x[dtree.attr_idx])
+
+            # If there isn't a correct outgoing edge
+            # then just return the majority class
+            if not child:
+                return dtree.cls
+            else:
+                return DecisionTree._predict(child, x)
+
+    def score(self, data):
+        """Find accuracy of dtree over data."""
+        predictions = [self._predict(self.root, x) for x in data]
+        return accuracy(data[:, 0], predictions)
+
+    def height(self):
+        return self._height(self.root)
+
+    @staticmethod
+    def _height(dtree):
+        if isinstance(dtree, Leaf):
+            return 0
+        else:
+            return 1 + max(map(DecisionTree._height, dtree.children.values()))
+
+    def node_count(self):
+        return self._node_count(self.root)
+
+    @staticmethod
+    def _node_count(dtree):
+        if isinstance(dtree, Leaf):
+            return 1
+        else:
+            return 1 + sum(map(DecisionTree._node_count, dtree.children.values()))
