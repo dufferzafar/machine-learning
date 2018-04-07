f86e2acefd545ddece040871ee67cdf0dfc14a98
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-07 21:47:20 +0530

    Pep8 etc

--- 3/visualization.py
+++ 3/visualization.py
@@ -4,16 +4,17 @@ This script gives you the plotting functions for part (b) for Neural net part of
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_decision_boundary(model, X, y):
+
+def plot_decision_boundary(model, X, y, title):
     """
     Given a model(a function) and a set of points(X), corresponding labels(y), scatter the points in X with color coding
-    according to y. Also use the model to predict the label at grid points to get the region for each label, and thus the 
+    according to y. Also use the model to predict the label at grid points to get the region for each label, and thus the
     descion boundary.
     Example usage:
     say we have a function predict(x,other params) which makes 0/1 prediction for point x and we want to plot
     train set then call as:
     plot_decision_boundary(lambda x:predict(x,other params),X_train,Y_train)
-    params(3): 
+    params(3):
         model : a function which expectes the point to make 0/1 label prediction
         X : a (mx2) numpy array with the points
         y : a (mx1) numpy array with labels
@@ -25,11 +26,17 @@ def plot_decision_boundary(model, X, y):
     h = 0.01
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
     # Predict the function value for the whole grid
     Z = model(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-    plt.ylabel('x2')
-    plt.xlabel('x1')
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title(title)
+
+    plt.savefig(title + ".png")
+    plt.close()
