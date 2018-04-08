ab7ba102a00c2a50be135a2e2d329ab7bf1d45ba
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-09 01:21:36 +0530

    NN: Driving code for MNIST part

--- 3/q2_nn.py
+++ 3/q2_nn.py
@@ -6,6 +6,8 @@ Part B: Toy dataset
 Part C: MNIST dataset
 """
 
+import csv
+
 import numpy as np
 
 # from matplotlib import pyplot as plt
@@ -16,21 +18,39 @@ from sklearn.linear_model import LogisticRegression
 
 from neural_network import NeuralNetwork
 
+from common import normalize
 from visualization import plot_decision_boundary
 
 
 def read_toy_data(neural=False):
-        trX = np.loadtxt("data/toy_trainX.csv", delimiter=",")
-        trY = np.loadtxt("data/toy_trainY.csv", delimiter=",")
-        tsX = np.loadtxt("data/toy_testX.csv", delimiter=",")
-        tsY = np.loadtxt("data/toy_testY.csv", delimiter=",")
+    trX = np.loadtxt("data/toy_trainX.csv", delimiter=",")
+    trY = np.loadtxt("data/toy_trainY.csv", delimiter=",")
+    tsX = np.loadtxt("data/toy_testX.csv", delimiter=",")
+    tsY = np.loadtxt("data/toy_testY.csv", delimiter=",")
 
-        # Convert data in a form suitable for neural network?
-        if neural:
-            trX = np.array([x.reshape(-1, 1) for x in trX])
-            tsX = np.array([x.reshape(-1, 1) for x in tsX])
+    # Convert data in a form suitable for neural network?
+    if neural:
+        trX = np.array([x.reshape(-1, 1) for x in trX])
+        tsX = np.array([x.reshape(-1, 1) for x in tsX])
 
-        return trX, trY, tsX, tsY
+    return trX, trY, tsX, tsY
+
+
+def read_mnist_data(fn):
+    x, y = [], []
+
+    # The CSV contains an image per row
+    # with the first 784 numbers representing 28x28 pixels
+    # and the last value representing the class label
+    with open(fn + ".csv") as f:
+        for row in csv.reader(f, delimiter=','):
+            y.append(int(row[-1]))
+            x.append([int(n) for n in row[:-1]])
+
+    X = normalize(x)
+    X = np.array([x.reshape(-1, 1) for x in X])
+
+    return X, np.asarray(y)
 
 
 def part_b_1():
@@ -62,7 +82,7 @@ def part_b_2():
 
     nn = NeuralNetwork(topo=[2, 5, 1])
 
-    nn.train(toy_trainX, toy_trainY, epochs=5000, eta=0.1)
+    nn.train(toy_trainX, toy_trainY, epochs=5000, eta=1)
 
     print("Training Accuracy", 100 * nn.score(toy_trainX, toy_trainY))
     print("Testing Accuracy", 100 * nn.score(toy_testX, toy_testY))
@@ -81,13 +101,15 @@ def part_b_3():
     print()
     print("Part B 3")
 
+    # TODO: Not working with a single hidden unit - 2 1 1
+    # for hidden_units in [2, 3, 10, 20, 40]:
     for hidden_units in [2, 3, 10, 20, 40]:
 
         print()
         print("Hidden units %d" % hidden_units)
 
         nn = NeuralNetwork(topo=[2, hidden_units, 1])
-        nn.train(toy_trainX, toy_trainY, epochs=5000, eta=0.1)
+        nn.train(toy_trainX, toy_trainY, epochs=5000, eta=1)
 
         print("Training Accuracy", 100 * nn.score(toy_trainX, toy_trainY))
         print("Testing Accuracy", 100 * nn.score(toy_testX, toy_testY))
@@ -107,15 +129,55 @@ def part_b_4():
 
     nn = NeuralNetwork(topo=[2, 5, 5, 1])
 
-    nn.train(toy_trainX, toy_trainY, epochs=5000, eta=0.2)
+    nn.train(toy_trainX, toy_trainY, epochs=5000, eta=1)
 
     print("Training Accuracy", 100 * nn.score(toy_trainX, toy_trainY))
     print("Testing Accuracy", 100 * nn.score(toy_testX, toy_testY))
 
     plot_decision_boundary(nn.predict, toy_trainX, toy_trainY,
-                           "Toy Training Data")
+                           "Toy Training Data", "b1_train_5_5")
     plot_decision_boundary(nn.predict, toy_testX, toy_testY,
-                           "Toy Testing Data")
+                           "Toy Testing Data", "b1_test_5_5")
+
+
+def part_c_1():
+    # LIBSVM Linear + Single Perceptron
+
+    print()
+    print("Part C 1")
+
+    print("Reading MNIST Data")
+    trainX, trainY = read_mnist_data("data/MNIST_train")
+    testX, testY = read_mnist_data("data/MNIST_test")
+
+    print("Architecture: 784 - 1")
+    nn = NeuralNetwork(topo=[784, 1])
+    nn.train(trainX, trainY, epochs=100, eta=3)
+
+    # TODO: Fix scoring for single perceptron case
+
+    print("Training Accuracy", 100 * nn.score(trainX, trainY))
+    print("Testing Accuracy", 100 * nn.score(testX, testY))
+
+
+def part_c_2():
+
+    print()
+    print("Part C 2")
+
+    print("Reading MNIST Data")
+    trainX, trainY = read_mnist_data("data/MNIST_train")
+    testX, testY = read_mnist_data("data/MNIST_test")
+
+    # TODO: Adpative eta
+    # TODO: Stopping criteria based on change in value of error function
+
+    print("Architecture: 784 - 100 - 10")
+    nn = NeuralNetwork(topo=[784, 100, 10])
+    nn.train(trainX, trainY, epochs=10, eta=3)
+
+    print("Training Accuracy", 100 * nn.score(trainX, trainY))
+    print("Testing Accuracy", 100 * nn.score(testX, testY))
 
 
 if __name__ == '__main__':
