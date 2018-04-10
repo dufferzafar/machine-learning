adb0b5e4121b10ef63dd0c003bca518f3436516d
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-10 15:22:17 +0530

    Copy function to convert data into libsvm format

--- 3/common.py
+++ 3/common.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 
 
@@ -22,3 +23,30 @@ def normalize(data):
         X = np.nan_to_num(X, copy=False)
 
     return X
+
+
+def svm_convert_data(input_file):
+    """Convert our data set into a format that libsvm can read."""
+
+    def read_data(fn):
+        y, x = [], []
+        with open(fn + ".csv") as f:
+            for row in csv.reader(f, delimiter=','):
+                y.append(int(row[-1]))
+                x.append([int(n) for n in row[:-1]])
+        return y, x
+
+    with open(input_file + "_svm", "w") as out:
+        for y, x in zip(*read_data(input_file)):
+            out.write("%d " % y)
+            out.write(" ".join(
+                ["%d:%d" % (i, x) for i, x in enumerate(x) if x])
+            )
+            out.write("\n")
+
+
+# if __name__ == '__main__':
+
+#     print("Converting data into a format that SVM can read")
+#     svm_convert_data("data/MNIST_test")
+#     svm_convert_data("data/MNIST_train")
