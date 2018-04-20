97d1f5ff946aa4b355d82c89c8116afc1f106724
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-20 23:52:17 +0530

    PCA: Plot components vs variance

--- 4/q1_b_pca_svm.py
+++ 4/q1_b_pca_svm.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -11,7 +12,23 @@ trX, trY, tsX = load_data()
 classes, trYi = np.unique(trY, return_inverse=True)
 
 
-def pca_svm():
+def pca_plot():
+    print("Fitting PCA")
+    pca = PCA().fit(trX)
+
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+
+    plt.xlabel("Number of components")
+    plt.ylabel("Cumulative explained variance")
+
+    plt.title("Deciding the number of components")
+
+    # plt.show()
+    plt.savefig("output/q1_b_pca.png")
+    plt.close()
+
+
+def pca_svm_linear():
 
     pipeline = Pipeline([
         # Normalize data to zero mean & unit variance
