dae5de41827f85cd69a45f7a2d45676702f85258
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-20 23:51:56 +0530

    Tried out LogReg

--- 4/others.py
+++ 4/others.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from xgboost import XGBClassifier
 
+from sklearn.linear_model import LogisticRegression
+
 from common import load_data, write_csv, accuracy
 
 trX, trY, tsX = load_data()
@@ -9,7 +11,8 @@ classes, trYi = np.unique(trY, return_inverse=True)
 
 
 def xgb():
-    gbm = XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05)
+    print("Training an XGB Classifier")
+    gbm = XGBClassifier()
     gbm.fit(trX, trYi)
 
     # Find training accuracy
@@ -19,3 +22,21 @@ def xgb():
     # Dump test labels
     tsP = classes[gbm.predict(tsX)]
     write_csv("xgb.csv", tsP)
+
+
+def logreg():
+    # Takes ~46 minutes to finish
+    print("Training Logistic Regression")
+    clf = LogisticRegression()
+    clf.fit(trX, trY)
+
+    print("Training Accuracy:", clf.score(trX, trY))
+
+    tsP = clf.predict(tsX)
+    write_csv("logreg.csv", tsP)
+
+
+if __name__ == '__main__':
+    xgb()
+    # logreg()
+    # voting_ensemble()
