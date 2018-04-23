e92bd7db4682c8f499a03ae739941588ede260a3
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-23 23:07:50 +0530

    Ensemble file voting...

--- 4/file_vote.py
+++ 4/file_vote.py
@@ -6,10 +6,16 @@ from common import write_csv
 
 # Various algorithms and their scores on the test set
 files = {
-    "kmeans_20.csv": 0.35220,
-    "pca_50_svm_rbf_C1.csv": 0.81892,
+    "keras_deeper_cnn.csv": 0.90270,
+    "keras_mnist_cnn.csv": 0.87797,
+    "conv_net_854.csv": 0.85422,
+    "pca_200_svm_rbf.csv": 0.83005,
+    # "conv_net_0.7_845.csv": 0.84555,
+    # "neural_net_500.csv": 0.78410,
+    # "logreg.csv": 0.63092,
+    # "kmeans_20.csv": 0.35220,
+    # "pca_50_svm_rbf_C1.csv": 0.81892,
     # "pca_50_svm_linear.csv": 0.69345,
-    "logreg.csv": 0.63092,
     # "xgb.csv": 0.62302,
 }
 
@@ -32,6 +38,8 @@ def soft_file_vote(files):
         label, _ = c.most_common(1)[0]
 
         yield label
+
+# def correlations():
 
 
 if __name__ == '__main__':
