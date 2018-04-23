8762bf3acc5a6776949c927517fc1228515ad7bc
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-23 23:08:17 +0530

    Tried tuning XGB (didn't work out)

--- 4/others.py
+++ 4/others.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from xgboost import XGBClassifier
 
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 from common import load_data, write_csv, accuracy
@@ -9,11 +10,21 @@ from common import load_data, write_csv, accuracy
 trX, trY, tsX = load_data()
 classes, trYi = np.unique(trY, return_inverse=True)
 
+print(len(tsX))
+
 
 def xgb():
     print("Training an XGB Classifier")
-    gbm = XGBClassifier()
-    gbm.fit(trX, trYi)
+
+    params = {
+        "max_depth": 5,
+        "n_setimators": 150,
+    }
+
+    trX_, tvX_, trY_, tvY_ = train_test_split(trX, trYi, test_size=0.3)
+
+    gbm = XGBClassifier(**params)
+    gbm.fit(trX_, trY_, eval_set=[(tvX_, tvY_)], verbose=True)
 
     # Find training accuracy
     trP = classes[gbm.predict(trX)]
@@ -21,7 +32,7 @@ def xgb():
 
     # Dump test labels
     tsP = classes[gbm.predict(tsX)]
-    write_csv("xgb.csv", tsP)
+    write_csv("xgb_d5_n150.csv", tsP)
 
 
 def logreg():
@@ -39,4 +50,3 @@ def logreg():
 if __name__ == '__main__':
     xgb()
     # logreg()
-    # voting_ensemble()
