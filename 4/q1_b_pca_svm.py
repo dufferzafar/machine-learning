81cdd9a0c8764e2a274efdf90d6cad34c0ebff53
Shadab Zafar <dufferzafar0@gmail.com>
2018-04-27 13:27:10 +0530

    4: Cleanup PCA + SVM; add grid search over C

--- 4/q1_b_pca_svm.py
+++ 4/q1_b_pca_svm.py
@@ -6,14 +6,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
+from sklearn.model_selection import GridSearchCV
+
 from common import load_data, write_csv
 
 trX, trY, tsX = load_data()
 classes, trYi = np.unique(trY, return_inverse=True)
 
+svm_pipeline = Pipeline([
+    # Normalize data to zero mean & unit variance
+    ('scaler', StandardScaler()),
+
+    # Project data down to 50 principal components
+    ('pca', PCA(n_components=50)),
+
+    # Learn a hyperplane in that space
+    ('clf', SVC(kernel="linear",
+                decision_function_shape="ovo")),
+])
+
 
 def pca_plot():
-    print("Fitting PCA")
+
+    print("\nFitting PCA")
     pca = PCA().fit(trX)
 
     plt.plot(np.cumsum(pca.explained_variance_ratio_))
@@ -23,48 +38,61 @@ def pca_plot():
 
     plt.title("Deciding the number of components")
 
-    # plt.show()
     plt.savefig("output/q1_b_pca.png")
     plt.close()
 
 
 def pca_svm_linear():
 
-    pipeline = Pipeline([
-        # Normalize data to zero mean & unit variance
-        ('scaler', StandardScaler()),
+    print(svm_pipeline)
 
-        # Project data down to 50 principal components
-        ('pca', PCA(n_components=50)),
+    print("\nFitting PCA (50) + SVM (Linear)")
+    svm_pipeline.fit(trX, trY)
 
-        # Learn a hyperplane in that space
-        ('clf', SVC(kernel="linear")),
-    ])
+    print("Training Accuracy: ", svm_pipeline.score(trX, trY))
 
-    print("Fitting PCA (50) + SVM (Linear)")
-    pipeline.fit(trX, trY)
-
-    print("Training Accuracy: ", pipeline.score(trX, trY))
-
-    tsP = pipeline.predict(tsX)
+    tsP = svm_pipeline.predict(tsX)
     write_csv("pca_50_svm_linear.csv", tsP)
 
 
+def pca_svm_linear_grid_search():
+
+    print(svm_pipeline)
+
+    print("\nRunning a grid search over C\n")
+
+    params = dict(clf__C=[0.1, 1, 10, 100])
+
+    grid_search = GridSearchCV(svm_pipeline,
+                               param_grid=params, verbose=True)
+
+    grid_search.fit(trX, trY)
+
+    print("Best Parameter:", grid_search.best_params_)
+    print("Best CV Score:", grid_search.best_score_)
+
+
 def pca_svm_rbf():
-    pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('pca', PCA(n_components=200)),
-        ('clf', SVC(kernel="rbf")),
-    ])
 
-    print("Fitting PCA + SVM")
-    pipeline.fit(trX, trY)
+    svm_pipeline.set_params(pca__n_components=250)
+    svm_pipeline.set_params(clf__kernel="rbf")
 
-    print("Training Accuracy: ", pipeline.score(trX, trY))
+    print(svm_pipeline)
 
-    tsP = pipeline.predict(tsX)
+    print("\nFitting PCA (250) + SVM (RBF)")
+    svm_pipeline.fit(trX, trY)
+
+    print("Training Accuracy: ", svm_pipeline.score(trX, trY))
+
+    tsP = svm_pipeline.predict(tsX)
     write_csv("pca_250_svm_rbf.csv", tsP)
 
 
 if __name__ == '__main__':
-    pca_svm()
+    # pca_plot()
+
+    pca_svm_linear()
+
+    # pca_svm_linear_grid_search()
+
+    # pca_svm_rbf()
