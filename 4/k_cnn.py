e035831d70e2099a5913392844e327c1469a7f17
Shadab Zafar <dufferzafar0@gmail.com>
2018-05-01 20:37:48 +0530

    Keras refactor - move architectures outside etc.

--- 4/k_cnn.py
+++ 4/k_cnn.py
@@ -11,31 +11,29 @@ trX, trY, tsX = load_data()
 classes, trYi = np.unique(trY, return_inverse=True)
 
 
-def keras_load_data(trX, trYi, tsX):
+def keras_load_data_split(trX, trYi, tsX, clean=True):
 
+    # Image Cleaning via Erosion
+    if clean:
+        kernel = np.ones((2, 1))
+        trX = np.array([cv2.erode(img, kernel) for img in trX]).reshape(trX.shape)
+        tsX = np.array([cv2.erode(img, kernel) for img in tsX]).reshape(tsX.shape)
+
+    # zero mean, unit variance
+    trX = normalize(trX)
+    tsX = normalize(tsX)
+
+    # Split datasets
     x_test = tsX
     x_train, x_val, y_train, y_val = train_test_split(
         trX, trYi, test_size=0.3
     )
 
-    if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, 28, 28)
-        x_val = x_val.reshape(x_val.shape[0], 1, 28, 28)
-        x_test = x_test.reshape(x_test.shape[0], 1, 28, 28)
-        input_shape = (1, 28, 28)
-    else:
-        x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-        x_val = x_val.reshape(x_val.shape[0], 28, 28, 1)
-        x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-        input_shape = (28, 28, 1)
-
-    x_train = x_train.astype('float32')
-    x_val = x_val.astype('float32')
-    x_test = x_test.astype('float32')
-
-    x_train /= 255
-    x_val /= 255
-    x_test /= 255
+    # Reshape
+    input_shape = (28, 28, 1)
+    x_train = x_train.reshape(x_train.shape[0], *input_shape)
+    x_val = x_val.reshape(x_val.shape[0], *input_shape)
+    x_test = x_test.reshape(x_test.shape[0], *input_shape)
 
     print('x_train shape:', x_train.shape)
 
@@ -47,100 +45,56 @@ def keras_load_data(trX, trYi, tsX):
     y_train = keras.utils.to_categorical(y_train, 20)
     y_val = keras.utils.to_categorical(y_val, 20)
 
-    return input_shape, x_train, y_train, x_val, y_val, x_test
+    return x_train, y_train, x_val, y_val, x_test
 
 
-def keras_mnist_cnn(input_shape):
+def keras_load_data(trX, trYi, tsX, clean=True):
 
-    net = Sequential()
+    # Image Cleaning via Erosion
+    if clean:
+        kernel = np.ones((2, 1))
+        trX = np.array([cv2.erode(img, kernel) for img in trX]).reshape(trX.shape)
+        tsX = np.array([cv2.erode(img, kernel) for img in tsX]).reshape(tsX.shape)
 
-    net.add(Conv2D(32, kernel_size=(3, 3),
-                   activation='relu',
-                   input_shape=input_shape))
+    # zero mean, unit variance
+    trX = normalize(trX)
+    tsX = normalize(tsX)
 
-    net.add(Conv2D(64, (3, 3), activation='relu'))
-    net.add(MaxPooling2D(pool_size=(2, 2)))
-    net.add(Dropout(0.25))
+    # Reshape
+    input_shape = (28, 28, 1)
+    trX = trX.reshape(trX.shape[0], *input_shape)
+    tsX = tsX.reshape(tsX.shape[0], *input_shape)
+    trYc = keras.utils.to_categorical(trYi, 20)
 
-    net.add(Flatten())
+    print(trX.shape[0], 'training samples')
+    print(tsX.shape[0], 'test samples')
 
-    net.add(Dense(128, activation='relu'))
-    net.add(Dropout(0.5))
-
-    net.add(Dense(20, activation='softmax'))
-
-    return net
-
-
-def keras_deeper_cnn(input_shape):
-
-    net = Sequential()
-
-    net.add(Conv2D(64, (8, 8), activation='relu',
-                   input_shape=input_shape))
-
-    net.add(Conv2D(192, (5, 5), activation='relu'))
-    net.add(MaxPooling2D(pool_size=(2, 2)))
-    net.add(Dropout(0.2))
-
-    net.add(Conv2D(384, (2, 2), activation='relu'))
-    net.add(Conv2D(256, (2, 2), activation='relu'))
-    net.add(Conv2D(256, (2, 2), activation='relu'))
-
-    net.add(Flatten())
-
-    net.add(Dropout(0.3))
-    net.add(Dense(2048, activation='relu'))
-
-    net.add(Dropout(0.5))
-    net.add(Dense(2048, activation='relu'))
-
-    net.add(Dense(20, activation='softmax'))
-
-    return net
+    return trX, trYc, tsX
 
 
-def keras_basic_cnn(input_shape):
-
-    net = Sequential()
-
-    net.add(Conv2D(32, kernel_size=(5, 5),
-                   activation='relu',
-                   input_shape=input_shape))
-
-    net.add(MaxPooling2D(pool_size=(4, 4)))
-
-    net.add(Flatten())
-
-    net.add(Dense(128, activation='relu'))
-    net.add(Dense(20, activation='softmax'))
-
-    return net
-
-
-if __name__ == '__main__':
+def train_keras_cnn(arch=keras_vgg_13):
 
     batch_size = 128
-    epochs = 25
+    epochs = 8
 
-    input_shape, x_train, y_train, x_val, y_val, x_test = keras_load_data(
-        trX, trYi, tsX
+    x_train, y_train, x_val, y_val, x_test = keras_load_data(
+        trX, trYi, tsX, normal=False
     )
 
-    # net = keras_deeper_cnn(input_shape)
-    # net = keras_mnist_cnn(input_shape)
-    net = keras_basic_cnn(input_shape)
+    net = arch()
+
+    print(net.summary())
 
     net.compile(
         loss=keras.losses.categorical_crossentropy,
-        optimizer=keras.optimizers.Adadelta(),
+        optimizer=keras.optimizers.Adam(),
         metrics=['accuracy']
     )
 
     callbacks_list = [
         keras.callbacks.EarlyStopping(
-            monitor='val_acc', min_delta=0.0001,
-            patience=3, verbose=1, mode='auto'
+            monitor='val_acc', min_delta=0.001,
+            patience=4, verbose=1, mode='auto'
         )
     ]
 
@@ -154,4 +108,14 @@ if __name__ == '__main__':
     )
 
     tsP = classes[net.predict_classes(x_test)]
-    write_csv("keras_deeper_cnn.csv", tsP)
+    write_csv("keras_vgg_13_cnn.csv", tsP)
+
+
+def load_model(model_file):
+    pass
+
+def save_model(net):
+    pass
+
+if __name__ == '__main__':
+    train_keras_model()
